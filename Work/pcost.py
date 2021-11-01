36bc3e20df2e22e7453adda26e3f1d771eb899f8
dlprice8 <dlprice8@gmail.com>
2021-11-01 12:27:20 -0400

    Complete Lesson 1.7

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,13 +2,30 @@
 #
 # Exercise 1.27
 
-Total_Cost = 0
+import csv
+import sys
 
-with open('Data/portfolio.csv','rt') as f:
-    headers = next(f)
-    for line in f:
-        row = line.split(',')
-        shares = int(row[1])
-        price = float(row[2])
-        Total_Cost += shares * price
-    print('Total Cost:', Total_Cost)
+def portfolio_cost(filename):
+    Total_Cost = 0
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        line_num = 0
+        for line in rows:
+            line_num += 1
+            row = line
+            try:
+                shares = int(row[1])
+            except ValueError:
+                print(f"Missing value on line {line_num}: {line}")
+            price = float(row[2])
+            Total_Cost += shares * price
+        return Total_Cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total Cost:', cost)
